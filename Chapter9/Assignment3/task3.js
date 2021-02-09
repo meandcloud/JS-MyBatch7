6e2159961dfd2368931bd12b2221059c2a1d1a2d
meandcloud <77702438+meandcloud@users.noreply.github.com>
2021-02-08 19:18:38 -0600

    added assignment 3
    added assignment 3 chapter 9

---
+++ Chapter9/Assignment3/task3.js
@@ -0,0 +1,5 @@
+var fisrtName = prompt("Please enter your first name: ");
+var lastName = prompt("Please enter your last name: ");
+var age = parseInt(prompt("Enter your age: "))
+
+alert("Hello "+fisrtName+" "+lastName+", "+"You are "+age+" year old")
