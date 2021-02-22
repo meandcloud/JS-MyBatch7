abb7ab27b9c1d362b5d8032c7a065660458e11fe
meandcloud <77702438+meandcloud@users.noreply.github.com>
2021-02-22 16:47:44 -0600

    added improved version in assignment 1 and 3
    added improved version in assignment 1 and 3

--- Chapter1-2-3/Assignment1/task1.js
+++ Chapter1-2-3/Assignment1/task1.js
@@ -1,4 +1,23 @@
 var firstName = "Amar";
-var lastName = "Shabbir";
-var age = 30;
+var lastName = "Shabbir"
+var age = 20;
+
 alert("Hello"+" "+ firstName+" " + lastName+" You are "+age+" year old.")
+
+function studentRecord(){
+    var inputVal1 = document.getElementById("rollNumber").value 
+    var inputVal2 = document.getElementById("program").value 
+    var inputVal3 = document.getElementById("fname").value 
+    var inputVal4 = document.getElementById("lname").value 
+    var inputVal5 = document.getElementById("email").value 
+    var inputVal6 = document.getElementById("phone").value 
+
+    document.getElementById("showRoll").innerHTML = inputVal1
+    document.getElementById("pro").innerHTML = inputVal2
+    document.getElementById("firstname").innerHTML = inputVal3
+    document.getElementById("lastname").innerHTML = inputVal4
+    document.getElementById("myemail").innerHTML = inputVal5
+    document.getElementById("num").innerHTML = inputVal6
+
+}
+
--- Chapter9/Assignment3/task3.js
+++ Chapter9/Assignment3/task3.js
@@ -3,3 +3,13 @@ var lastName = prompt("Please enter your last name: ");
 var age = parseInt(prompt("Enter your age: "))
 
 alert("Hello "+fisrtName+" "+lastName+", "+"You are "+age+" year old")
+
+//Added Improved Version:
+
+function message(){
+    var inputVal1 = document.getElementById("fname").value;
+    var inputVal2 = document.getElementById("lname").value;
+    var inputVal3 = document.getElementById("age").value;
+
+    document.getElementById("displayMsg").innerHTML = "Hello "+ inputVal1 +" "+ inputVal2 +" You are "+ inputVal3+ " years old";
+}
